f514beac42e239fde83021dcaae9f98ad6fbd169
Vinicius Serpa <vinicius-serpa@live.com>
2022-10-13 08:14:52 -0300

    path refactoring and create queue interface

---
+++ foundation/ds/queue.js
@@ -0,0 +1,37 @@
+// FIFO - First in, first out
+
+function Queue() {
+
+    var items = [];
+
+    // add element to queue
+    this.enqueue = function(element) {
+
+    }
+
+    // remove element from queue
+    this.dequeue = function() {
+
+    }
+
+    // return the first element of queue
+    this.front = function() {
+
+    }
+
+    // return true if queue is empty
+    this.isEmpty = function() {
+
+    }
+
+    // return the queue length
+    this.size = function() {
+
+    }
+
+    // print the queue
+    this.print = function() {
+
+    }
+
+}
