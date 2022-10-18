571636003114324d53cf23b933b46b3582d8e4fc
Vinicius Serpa <vinicius-serpa@live.com>
2022-10-18 15:29:56 -0300

    linked list interface

--- README.md
+++ README.md
@@ -6,9 +6,16 @@
 - Stack
 - Queue
 - Priority Queue
+- Linked List
 
 ## Algorithms
 
 - Dec2Bin (Stack)
 - Dec2Base (Stack)
 - HotPotato (Circular Queue)
+
+## Notes
+
+### Linked List
+
+Lista ligada é uma coleção sequencial de elementos mas, diferente dos arrays, não são alocados de forma contígua na memória. Cada elemento é constituído de um nó, que armazena tanto o valor como uma referência ao próximo elemento (ponteiro).
---
+++ foundation/ds/linked-list.js
@@ -0,0 +1,55 @@
+function LinkedList() {
+
+    var Node = function(element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    var length = 0;
+    var head = null;
+
+    // add an element on the end of the list
+    this.append = function(element) {
+
+    }
+
+    // add an element in a specific position
+    this.insert = function(position, element) {
+
+    }
+
+    // remove element from a specific position
+    this.removeAt = function(position) {
+
+    }
+
+    // remove a element
+    this.remove = function(element) {
+
+    }
+
+    // return the element position
+    this.indexOf = function(element) {
+
+    }
+
+    // return true if the list is empty
+    this.isEmpty = function() {
+
+    }
+
+    // return the size of list
+    this.size = function() {
+
+    }
+
+    // convert the list to a string
+    this.toString = function() {
+
+    }
+
+    // print on console
+    this.print = function() {
+        
+    }
+}
