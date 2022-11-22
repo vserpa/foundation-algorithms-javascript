34f4656c0d9a8b7946a1a190bc93e3028b82c082
Vinicius Serpa <vinicius-serpa@live.com>
2022-11-22 17:09:09 -0300

    set implementation

--- foundation/ds/set.js
+++ foundation/ds/set.js
@@ -1,28 +1,59 @@
 function Set() {
-    
+
     var items = {};
 
     this.add = function(value) {
+        if (this.has(value)) {
+            return false;
+        }
 
+        items[value] = value;
+        return true;
     }
 
     this.delete = function(value) {
+        if (!this.has(value)) {
+            return false;
+        }
 
+        delete items[value];
+        return true;
     }
 
     this.has = function(value) {
-
+        return items.hasOwnProperty(value);
     }
 
     this.clear = function() {
-
+        items = {};
     }
 
     this.size = function() {
-
+        return Object.keys(items).length;
     }
 
     this.values = function() {
-
+        var values = [];
+        var keys = Object.keys(items);
+        for (var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]]);
+        }
+        return values;
     }
 }
+
+var set = new Set();
+
+set.add(1);
+set.add(2);
+set.add(3);
+set.add(4);
+set.add(4);
+set.add(4);
+
+// set.clear();
+set.delete(2);
+
+console.log(set.has(3))
+console.log(set.size());
+console.log(set.values());
